e36cfe45ca8a28a587c2d08c76e9f913a14f5a51
TENCENT\carlyin <carlyin@tencent.com>
2019-03-06 16:42:08 +0800

    sdk集成蓝盾api接口

---
+++ src/main/java/com/tencent/bk/devops/atom/api/impl/ArtifactoryApi.java
@@ -0,0 +1,40 @@
+package com.tencent.bk.devops.atom.api.impl;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.tencent.bk.devops.atom.api.BaseApi;
+import com.tencent.bk.devops.atom.pojo.Result;
+import com.tencent.bk.devops.atom.utils.json.JsonUtil;
+import okhttp3.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ArtifactoryApi extends BaseApi {
+
+    private final static Logger logger = LoggerFactory.getLogger(ArtifactoryApi.class);
+
+    @SuppressWarnings("all")
+    public  Result<List<String>> getArtifactoryFileUrl(String projectCode,String pipelineId,String buildId,String artifactoryType,String path){
+        StringBuilder urlBuilder = new StringBuilder("/artifactory/api/build/artifactories/project/");
+        urlBuilder.append(projectCode).append("/pipeline/").append(pipelineId).append("/buildId/").append(buildId).append("/getFileDownloadUrl?")
+                .append("artifactoryType=").append(artifactoryType).append("&path=").append(path);
+        String requestUrl = urlBuilder.toString();
+        logger.info("the requestUrl is:{}",requestUrl);
+        Request request = super.buildGet(urlBuilder.toString());
+        String responseContent = null;
+        try {
+            responseContent = super.request(request,"获取包路径失败");
+        } catch (IOException e) {
+            logger.error("get artifactoryFileUrl throw Exception", e);
+        }
+        logger.info("getArtifactoryFileUrl responseContent is:{}", responseContent);
+        if(null != responseContent){
+            return JsonUtil.fromJson(responseContent,new TypeReference<Result<List<String>>>(){});
+        }else{
+            return new Result(null);
+        }
+    }
+
+}
---
+++ src/main/java/com/tencent/bk/devops/atom/api/impl/PluginApi.java
@@ -0,0 +1,35 @@
+package com.tencent.bk.devops.atom.api.impl;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.tencent.bk.devops.atom.api.BaseApi;
+import com.tencent.bk.devops.atom.pojo.Result;
+import com.tencent.bk.devops.atom.pojo.plugin.OnsHostInfo;
+import com.tencent.bk.devops.atom.utils.json.JsonUtil;
+import okhttp3.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class PluginApi extends BaseApi {
+
+    private final static Logger logger = LoggerFactory.getLogger(PluginApi.class);
+
+    @SuppressWarnings("all")
+    public Result<OnsHostInfo> getOnsHostInfo(String domainName){
+        Request request = super.buildGet("/plugin/api/build/ons/host/domains/" + domainName);
+        String responseContent = null;
+        try {
+            responseContent = super.request(request,"获取请求IP地址信息失败");
+        } catch (IOException e) {
+            logger.error("getOnsHostInfo throw Exception", e);
+        }
+        logger.info("getOnsHostInfo responseContent is:{}", responseContent);
+        if(null != responseContent){
+            return JsonUtil.fromJson(responseContent,new TypeReference<Result<OnsHostInfo>>(){});
+        }else{
+            return new Result(null);
+        }
+    }
+
+}
