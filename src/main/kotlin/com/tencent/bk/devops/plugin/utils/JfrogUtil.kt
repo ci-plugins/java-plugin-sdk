1af24b9c18921cc98e422651c322aecd528a5daf
ajackyu <ajackyu@tencent.com>
2019-07-28 17:48:35 +0800

    增加自定义仓库和流水线指定仓库对应构建历史下文件

--- src/main/kotlin/com/tencent/bk/devops/plugin/utils/JfrogUtil.kt
+++ src/main/kotlin/com/tencent/bk/devops/plugin/utils/JfrogUtil.kt
@@ -9,6 +9,14 @@ import java.nio.file.Paths
 class JfrogUtil {
     private val jfrogResourceApi = JfrogResourceApi()
 
+    /**
+     * 根据输入匹配流水线仓库或者自定义仓库对应的文件
+     * @param runningBuildId  当前流水线构建号对应的pipelineBuildId
+     * @param srcPath         待匹配的文件路径，支持通配符
+     * @param pipelineId      流水线id如果为"",匹配自定义仓库，不为空则是匹配对应流水线仓库的id
+     * @param buildId         流水线构建id,用法同上
+     * @return 匹配到的文件集合
+     */
     fun matchFile(runningBuildId: String, srcPath: String, pipelineId: String = "", buildId: String = ""): List<JfrogFile> {
         val result = mutableListOf<JfrogFile>()
         val data = jfrogResourceApi.getAllFiles(runningBuildId, pipelineId, buildId)
