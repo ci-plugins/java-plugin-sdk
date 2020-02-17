64289ccba4839cd750a026588d5cf0f6e7c42a72
lj654548718 <654548718@qq.com>
2020-02-17 14:35:52 +0800

    add pojo default value

--- src/main/kotlin/com/tencent/bk/devops/plugin/pojo/artifactory/Property.kt
+++ src/main/kotlin/com/tencent/bk/devops/plugin/pojo/artifactory/Property.kt
@@ -3,7 +3,7 @@ package com.tencent.bk.devops.plugin.pojo.artifactory
 // ("版本仓库-元数据")
 data class Property(
     // ("元数据键", required = true)
-    val key: String,
+    val key: String = "",
     // ("元数据值", required = true)
-    val value: String
+    val value: String = ""
 )
