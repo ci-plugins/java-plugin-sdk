14a465129172cbdc080e0252a89b7ebb1b9a8fe9
necrohuang <necrohuang@tencent.com>
2020-03-16 18:48:30 +0800

    update downloadFile

--- src/main/kotlin/com/tencent/bk/devops/plugin/api/ArchiveApi.kt
+++ src/main/kotlin/com/tencent/bk/devops/plugin/api/ArchiveApi.kt
@@ -3,68 +3,59 @@ package com.tencent.bk.devops.plugin.api
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import org.apache.commons.io.FileUtils
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.FileOutputStream
 import java.util.concurrent.TimeUnit
-import kotlin.math.ceil
 
 class ArchiveApi {
 
     private val okHttpClient: OkHttpClient = okhttp3.OkHttpClient.Builder()
-            .connectTimeout(5L, TimeUnit.SECONDS)
-            .readTimeout(300 * 5L, TimeUnit.SECONDS) // Set to 15 minutes
-            .writeTimeout(60L, TimeUnit.SECONDS)
-            .build()
+        .connectTimeout(5L, TimeUnit.SECONDS)
+        .readTimeout(300 * 5L, TimeUnit.SECONDS) // Set to 15 minutes
+        .writeTimeout(60L, TimeUnit.SECONDS)
+        .build()
 
     public fun download(uri: String, request: Request, destPath: File, size: Long, bkWorkSpace: String) {
         okHttpClient.newBuilder().build().newCall(request).execute().use { response ->
-            if (!response.isSuccessful) throw RuntimeException("get $request failured......,please check your input")
-            download(uri, response, destPath, size, bkWorkSpace)
+            if (!response.isSuccessful) throw RuntimeException("get $request failed, response code: ${response.code()}")
+            download(response, destPath, size)
         }
     }
 
-    private fun download(uri: String, response: Response, destPath: File, size: Long, bkWorkSpace: String) {
+    private fun download(response: Response, destPath: File, size: Long) {
         if (response.code() == 404) {
             throw RuntimeException("文件不存在")
         }
         if (!response.isSuccessful) {
-//            LoggerService.addNormalLine(response.body()!!.string())
             throw RuntimeException("获取文件失败")
         }
         if (!destPath.parentFile.exists()) destPath.parentFile.mkdirs()
-//        LoggerService.addNormalLine("save file >>>> ${destPath.canonicalPath}")
-        logger.info("save file >>>> ${destPath.canonicalPath}")
-        var process = 0L
+        val target = destPath.canonicalPath
+        logger.info("save file to: $target")
+        var readBytes = 0L
+        var doubleSize = size.toDouble()
+        var startTime = System.currentTimeMillis()
         response.body()!!.byteStream().use { bs ->
             val buf = ByteArray(4096)
+            var logTime = System.currentTimeMillis()
             var len = bs.read(buf)
-            process = writeStateToTxt(process, size, bkWorkSpace, uri, len, true)
             FileOutputStream(destPath).use { fos ->
+                logger.info("$target > 0.0%\n")
                 while (len != -1) {
                     fos.write(buf, 0, len)
                     len = bs.read(buf)
-                    process = writeStateToTxt(process, size, bkWorkSpace, uri, len, false)
+                    readBytes += len
+                    val now = System.currentTimeMillis()
+                    if ((now - logTime) > 3000) {
+                        logger.info("$target > ${String.format("%.1f", readBytes / doubleSize)}%\n")
+                        logTime = now
+                    }
                 }
+                logger.info("$target > 100%\n")
             }
         }
-    }
-
-    private fun writeStateToTxt(process: Long, size: Long, bkWorkSpace: String, uri: String, len: Int, isFirst: Boolean): Long {
-        val l = process + len
-        val countResult = if (size != 0L) ceil(((process / size) * 100).toDouble()) else 0
-        val log = "$uri :$countResult%\n"
-        logger.info(log)
-        var txtFile = File("$bkWorkSpace${File.separator}BKCIArchiveDownLoadState.txt")
-        if (isFirst) {
-            if (txtFile.exists()) {
-                txtFile.delete()
-            }
-            txtFile.createNewFile()
-        }
-        FileUtils.write(txtFile, log, "UTF-8", true)
-        return l
+        logger.info("file transfer time: ${String.format("%.2f", (System.currentTimeMillis() - startTime) / 1000.0)} seconds")
     }
 
     companion object {
