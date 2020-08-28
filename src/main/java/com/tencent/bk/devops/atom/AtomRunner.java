2f6c63589b6eb71997c6b1be19529f0486d43fa5
royalhuang <royalhuang@tencent.com>
2020-08-28 11:27:40 +0800

    增加log归类子标签接口与errorType上报接口

--- src/main/java/com/tencent/bk/devops/atom/AtomRunner.java
+++ src/main/java/com/tencent/bk/devops/atom/AtomRunner.java
@@ -6,7 +6,6 @@ import com.tencent.bk.devops.atom.pojo.AtomBaseParam;
 import com.tencent.bk.devops.atom.spi.AtomService;
 import com.tencent.bk.devops.atom.spi.ServiceLoader;
 import com.tencent.bk.devops.atom.spi.TaskAtom;
-import com.tencent.bk.devops.plugin.pojo.DefaultErrorCode;
 import com.tencent.bk.devops.plugin.pojo.ErrorType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,8 +32,6 @@ public class AtomRunner {
             logger.error("Unknown Error：{}",e.getMessage());
             e.printStackTrace();
             context.getResult().setStatus(Status.error);
-            context.getResult().setErrorType(ErrorType.PLUGIN.getNum());
-            context.getResult().setErrorCode(DefaultErrorCode.PLUGIN_DEFAULT_ERROR);
             context.getResult().setMessage("Unknown Error：" + e.getMessage());
         } finally {
             context.persistent();
