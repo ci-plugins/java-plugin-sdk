ccf2e30850a8d39356b44d3cd5907bc2bac8d58f
linjun-ddddd <654548718@qq.com>
2020-10-22 14:42:56 +0800

    feat: 增加docker相关api fix StringUtils bug

--- src/main/java/com/tencent/bk/devops/atom/utils/http/OkHttpUtils.java
+++ src/main/java/com/tencent/bk/devops/atom/utils/http/OkHttpUtils.java
@@ -6,7 +6,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
