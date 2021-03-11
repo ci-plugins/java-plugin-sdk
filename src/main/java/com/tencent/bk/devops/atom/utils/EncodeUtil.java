a195a4c746c60b90cd806bb7517cd14bcb999c82
jsonwan <1334793794@qq.com>
2021-03-11 20:38:31 +0800

    --bug=85909415 插件java SDK下载特殊字符文件失败
    支持特殊字符的URL编码

--- src/main/java/com/tencent/bk/devops/atom/utils/EncodeUtil.java
+++ src/main/java/com/tencent/bk/devops/atom/utils/EncodeUtil.java
@@ -4,9 +4,9 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 public class EncodeUtil {
-    //判断汉字的方法,只要编码在\u4e00到\u9fa5之间的都是汉字
+    //判断汉字/特殊符号的方法,编码在\u0080到\uffff之间的都是汉字或其他特殊符号
     private static Boolean isChineseChar(Character ch) {
-        return ch.toString().matches("[\u4e00-\u9fa5]");
+        return ch.toString().matches("[\u0080-\uffff]");
     }
 
     public static String encodeChinese(String str) {
