c0f814c27324c675d5c93b00482de8506483c04f
sawyersong <sawyersong>
2021-04-26 16:35:14 +0800

    --story=864151815 【sdk api】支持创建容器

--- src/main/kotlin/com/tencent/bk/devops/plugin/api/impl/DevCloudBuildApi.kt
+++ src/main/kotlin/com/tencent/bk/devops/plugin/api/impl/DevCloudBuildApi.kt
@@ -2,13 +2,18 @@ package com.tencent.bk.devops.plugin.api.impl
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.tencent.bk.devops.atom.api.BaseApi
+import com.tencent.bk.devops.atom.utils.http.SdkUtils
 import com.tencent.bk.devops.atom.utils.json.JsonUtil
 import com.tencent.bk.devops.plugin.docker.pojo.job.response.JobResponse
 import com.tencent.bk.devops.plugin.docker.utils.EnvUtils
 import com.tencent.bk.devops.plugin.pojo.Result
 import com.tencent.bk.devops.plugin.pojo.devcloud.DevCloudJobReq
 import okhttp3.RequestBody
+import org.apache.commons.io.FileUtils
 import org.slf4j.LoggerFactory
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
 
 class DevCloudBuildApi : BaseApi() {
 
@@ -17,7 +22,7 @@ class DevCloudBuildApi : BaseApi() {
         devCloudJobReq.podNameSelector = EnvUtils.getHostName()
         val requestBody = RequestBody.create(JSON_CONTENT_TYPE, JsonUtil.toJson(devCloudJobReq))
         logger.info("create job request: $requestBody")
-        val request = buildPost(path, requestBody, mutableMapOf())
+        val request = buildPost(path, requestBody, mutableMapOf("X-DEVOPS-UID" to getUserId()))
         val responseContent = request(request, "创建devCloud job失败")
         logger.info("create job response: $responseContent")
         return JsonUtil.fromJson(responseContent, object : TypeReference<Result<JobResponse?>>() {})
@@ -25,7 +30,7 @@ class DevCloudBuildApi : BaseApi() {
 
     fun getJobStatus(jobName: String): Result<String> {
         val path = "/dispatch-devcloud/api/build/devcloud/job/" + jobName + "status"
-        val request = buildGet(path)
+        val request = buildGet(path, mutableMapOf("X-DEVOPS-UID" to getUserId()))
         val responseContent = request(request, "获取job状态失败")
         logger.info("get job status response: $responseContent")
         return JsonUtil.fromJson(responseContent, object : TypeReference<Result<String>>() {})
@@ -33,7 +38,7 @@ class DevCloudBuildApi : BaseApi() {
 
     fun getJobLogs(jobName: String): Result<String> {
         val path = "/dispatch-devcloud/api/build/devcloud/job/" + jobName + "logs"
-        val request = buildGet(path)
+        val request = buildGet(path, mutableMapOf("X-DEVOPS-UID" to getUserId()))
         val responseContent = request(request, "获取job日志失败")
         logger.info("get job logs response: $responseContent")
         return JsonUtil.fromJson(responseContent, object : TypeReference<Result<String>>() {})
@@ -41,10 +46,25 @@ class DevCloudBuildApi : BaseApi() {
 
     fun getTask(taskId: String): Result<String> {
         val path = "/dispatch-devcloud/api/build/devcloud/task/" + taskId
-        val request = buildGet(path)
+        val request = buildGet(path, mutableMapOf("X-DEVOPS-UID" to getUserId()))
         val responseContent = request(request, "获取task信息失败")
         logger.info("get task response: $responseContent")
         return JsonUtil.fromJson(responseContent, object : TypeReference<Result<String>>() {})
+    }
+
+    private fun getUserId(): String {
+        var inputJson: String? = null
+        try {
+            inputJson = FileUtils.readFileToString(
+                File(SdkUtils.getDataDir() + "/" + SdkUtils.getInputFile()),
+                Charset.defaultCharset()
+            )
+        } catch (e: IOException) {
+            logger.error("parse inputJson throw Exception", e)
+        }
+
+        val inputMap: Map<String, Any> = JsonUtil.fromJson(inputJson, object : TypeReference<MutableMap<String, Any>>() {})
+        return inputMap["pipeline.start.user.name"] as String
     }
 
     companion object {
