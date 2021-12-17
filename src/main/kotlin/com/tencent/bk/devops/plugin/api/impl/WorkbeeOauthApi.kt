8b76c94fba9f72a827ce1201812d597f254fb1e1
v_yjieliang <v_yjieliang@tencent.com>
2021-12-17 10:42:19 +0800

    修改getOauth方法，增加GitToken类

--- src/main/kotlin/com/tencent/bk/devops/plugin/api/impl/WorkbeeOauthApi.kt
+++ src/main/kotlin/com/tencent/bk/devops/plugin/api/impl/WorkbeeOauthApi.kt
@@ -2,9 +2,11 @@ package com.tencent.bk.devops.plugin.api.impl
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.tencent.bk.devops.atom.api.BaseApi
+import com.tencent.bk.devops.plugin.pojo.Result
+import com.tencent.bk.devops.plugin.pojo.repository.GitToken
 import com.tencent.bk.devops.plugin.utils.JsonUtil
 import org.slf4j.LoggerFactory
-import java.text.MessageFormat
+
 
 
 class WorkbeeOauthApi :BaseApi() {
@@ -16,13 +18,13 @@ class WorkbeeOauthApi :BaseApi() {
     /**
      *获取工蜂OAUTH信息
      */
-    fun getOauth(userId :String) : com.tencent.bk.devops.plugin.pojo.Result<Map<String, String>> {
+    fun getOauth(userId :String) : Result<GitToken?> {
 
-        val path :String=MessageFormat.format("/repository/api/build/oauth/git/{0}",userId)
+        val path="/repository/api/build/oauth/git/$userId"
         val request = buildGet(path)
         val responseContent= request(request, "获取Oauth信息失败")
 
-        return JsonUtil.to(responseContent, object : TypeReference<com.tencent.bk.devops.plugin.pojo.Result<Map<String, String>>>() {})
+        return JsonUtil.to(responseContent, object : TypeReference<Result<GitToken?>>() {})
 
     }
 
