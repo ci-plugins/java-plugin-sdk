3f828ddacc67732817a6e11fa1c1a4e6f5c039b6
mingshewhe <mingshewhe@tencent.com>
2022-04-20 14:26:56 +0800

    --story=873868149 java sdk BaseApi增加请求重试方法

--- src/main/java/com/tencent/bk/devops/atom/api/BaseApi.java
+++ src/main/java/com/tencent/bk/devops/atom/api/BaseApi.java
@@ -10,7 +10,12 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpRetryException;
+import java.net.SocketTimeoutException;
 import java.net.URLEncoder;
+import java.net.UnknownHostException;
+import java.rmi.ConnectException;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +23,82 @@ public class BaseApi {
 
     protected static final MediaType JSON_CONTENT_TYPE = MediaType.parse("application/json; charset=utf-8");
     private static final Logger logger = LoggerFactory.getLogger(BaseApi.class);
+    private static final long sleepTimeMills = 500L;
+    private static final int DEFAULT_RETRY_TIME = 5;
+
+    protected Response requestForResponse(Request request, int retryCount) throws IOException, InterruptedException {
+        OkHttpClient httpClient = okHttpClient.newBuilder().build();
+        boolean retryFlag;
+        try {
+            Response response = httpClient.newCall(request).execute();
+            if (Arrays.asList(502, 503, 504).contains(response.code())) {
+                retryFlag = true;
+            } else {
+                return response;
+            }
+        } catch (UnknownHostException e) {
+            logger.warn("UnknownHostException|request({}),error is :{}, try to retry {}",
+                    request, e.getMessage(), retryCount
+            );
+            retryFlag = retryCount > 0;
+        } catch (ConnectException e) {
+            logger.warn(
+                    "ConnectException|request({}),error is :{}, try to retry {}",
+                    request, e.getMessage(), retryCount
+            );
+            retryFlag = retryCount > 0;
+        } catch (SocketTimeoutException re) {
+            if ("connect timed out".equals(re.getMessage()) ||
+                    ("GET".equals(request.method()) && "timeout".equals(re.getMessage()))
+            ) {
+                logger.warn("SocketTimeoutException|request({}),error is :{}, try to retry {}",
+                        request, re.getMessage(), retryCount
+                );
+                retryFlag = retryCount > 0;
+            } else {
+                logger.error("Fail to request({})", request, re);
+                throw re;
+            }
+        } catch (Exception error) {
+            logger.error("Fail to request({})", request, error);
+            throw new RemoteServiceException(
+                    String.format("Fail to request(%s),error is:%s", request, error.getMessage()),
+                    500, ""
+            );
+        }
+        if (retryFlag && retryCount > 0) {
+            logger.warn(
+                    "Fail to request({}), retry after {} ms", request, sleepTimeMills
+            );
+            Thread.sleep(sleepTimeMills);
+            return requestForResponse(request, retryCount - 1);
+        } else {
+            logger.error("Fail to request({}), try to retry {}", request, retryCount);
+            throw new HttpRetryException(
+                    String.format("Fail to request(%s), try to retry %s", request, retryCount),
+                    999
+            );
+        }
+    }
+
+    protected String retryRequest(Request request, String errorMessage, int retryCount) throws IOException {
+        try (Response response = requestForResponse(request, retryCount)) {
+            String responseContent = response.body() != null ? response.body().string() : null;
+            if (!response.isSuccessful()) {
+                logger.error("Fail to request(" + request + ") with code " + response.code()
+                        + " , message " + response.message() + " and response" + responseContent);
+                logger.info("excep>>>>>>>>>>>>" + response);
+                throw new RemoteServiceException(errorMessage, response.code(), responseContent);
+            }
+            return responseContent;
+        } catch (InterruptedException e) {
+            throw new RemoteServiceException(errorMessage, 500, e.getMessage());
+        }
+    }
+
+    protected String retryRequest(Request request, String errorMessage) throws IOException {
+        return retryRequest(request, errorMessage, DEFAULT_RETRY_TIME);
+    }
 
     protected String request(Request request, String errorMessage) throws IOException {
         OkHttpClient httpClient = okHttpClient.newBuilder().build();
--- src/main/kotlin/com/tencent/bk/devops/plugin/api/impl/SensitiveConfApi.kt
+++ src/main/kotlin/com/tencent/bk/devops/plugin/api/impl/SensitiveConfApi.kt
@@ -1,12 +1,12 @@
 package com.tencent.bk.devops.plugin.api.impl
 
 import com.fasterxml.jackson.core.type.TypeReference
-import com.tencent.bk.devops.plugin.api.AbstractBaseApi
+import com.tencent.bk.devops.atom.api.BaseApi
 import com.tencent.bk.devops.plugin.pojo.Result
 import com.tencent.bk.devops.plugin.pojo.atom.SensitiveConfResp
 import com.tencent.bk.devops.plugin.utils.JsonUtil
 
-class SensitiveConfApi : AbstractBaseApi() {
+class SensitiveConfApi : BaseApi() {
 
     /**
      * 获取插件私有配置
