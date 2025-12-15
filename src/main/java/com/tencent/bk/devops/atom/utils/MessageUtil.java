9180bceb877689ff555caca8309b02c0aa489069
carlyin <carlyin@tencent.com>
2025-12-15 16:17:34 +0800

    sdk支持jdk17

--- src/main/java/com/tencent/bk/devops/atom/utils/MessageUtil.java
+++ src/main/java/com/tencent/bk/devops/atom/utils/MessageUtil.java
@@ -1,12 +1,10 @@
 package com.tencent.bk.devops.atom.utils;
 
-import kotlin.text.Charsets;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MessageUtil {
 
@@ -17,7 +15,7 @@ public class MessageUtil {
      * 根据语言环境获取对应的描述信息
      *
      * @param messageCode 消息标识
-     * @param language    语言信息
+     * @param language 语言信息
      * @return 描述信息
      */
     public static String getMessageByLocale(String messageCode, String language) {
@@ -27,12 +25,13 @@ public class MessageUtil {
     /**
      * 根据语言环境获取对应的描述信息
      *
-     * @param messageCode    消息标识
-     * @param language       语言信息
+     * @param messageCode 消息标识
+     * @param language 语言信息
      * @param defaultMessage 默认信息
      * @return 描述信息
      */
-    public static String getMessageByLocale(String messageCode, String language, String defaultMessage) {
+    public static String getMessageByLocale(
+            String messageCode, String language, String defaultMessage) {
         return getMessageByLocale(messageCode, language, null, DEFAULT_BASE_NAME, defaultMessage);
     }
 
@@ -40,8 +39,8 @@ public class MessageUtil {
      * 根据语言环境获取对应的描述信息
      *
      * @param messageCode 消息标识
-     * @param language    语言信息
-     * @param params      替换描述信息占位符的参数数组
+     * @param language 语言信息
+     * @param params 替换描述信息占位符的参数数组
      * @return 描述信息
      */
     public static String getMessageByLocale(String messageCode, String language, String[] params) {
@@ -51,50 +50,42 @@ public class MessageUtil {
     /**
      * 根据语言环境获取对应的描述信息
      *
-     * @param messageCode    消息标识
-     * @param language       语言信息
-     * @param params         替换描述信息占位符的参数数组
+     * @param messageCode 消息标识
+     * @param language 语言信息
+     * @param params 替换描述信息占位符的参数数组
      * @param defaultMessage 默认信息
      * @return 描述信息
      */
     public static String getMessageByLocale(
-        String messageCode,
-        String language,
-        String[] params,
-        String defaultMessage
-    ) {
+            String messageCode, String language, String[] params, String defaultMessage) {
         return getMessageByLocale(messageCode, language, params, DEFAULT_BASE_NAME, defaultMessage);
     }
 
     /**
      * 根据语言环境获取对应的描述信息
      *
-     * @param messageCode    消息标识
-     * @param language       语言信息
-     * @param params         替换描述信息占位符的参数数组
-     * @param baseName       基础资源名称
+     * @param messageCode 消息标识
+     * @param language 语言信息
+     * @param params 替换描述信息占位符的参数数组
+     * @param baseName 基础资源名称
      * @param defaultMessage 默认信息
      * @return 描述信息
      */
     public static String getMessageByLocale(
-        String messageCode,
-        String language,
-        String[] params,
-        String baseName,
-        String defaultMessage
-    ) {
+            String messageCode,
+            String language,
+            String[] params,
+            String baseName,
+            String defaultMessage) {
         // 通过resourceBundle获取对应语言的描述信息
         String message = null;
         try {
-            String[] parts = language.split("_");
-            Locale localeObj = new Locale(language);
-            if (parts.length > 1) {
-                localeObj = new Locale(parts[0], parts[1]);
-            }
+            String[] arrs = language.split("_");
+            Locale localeObj = new Locale(arrs[0], arrs[1]);
             // 根据locale和baseName生成resourceBundle对象
             ResourceBundle resourceBundle = ResourceBundle.getBundle(baseName, localeObj);
             // 通过resourceBundle获取对应语言的描述信息
-            message = new String(resourceBundle.getString(messageCode).getBytes(Charsets.ISO_8859_1), Charsets.UTF_8);
+            message = resourceBundle.getString(messageCode);
         } catch (Throwable ignored) {
             logger.warn("Fail to get i18nMessage of messageCode[" + messageCode + "]", ignored);
         }
